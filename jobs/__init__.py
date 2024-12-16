766755fc0eb62d480b4fca73110f1c8b86c8efd0
Carlos Huaccho <Carlos@gmail.com>
2024-12-16 15:44:11 -0500

    Adding jobs to init

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,1 +1,12 @@
 """Data population jobs."""
+
+import typing as t
+
+from nautobot.apps.jobs import register_jobs
+
+from .location_creation import LocationCreation
+
+jobs: list[t.Any] = [
+    LocationCreation,
+]
+register_jobs(*jobs)
